723740e2650fc2e493d483d343bd5b703b3dc38a
chandra-deb <chandra-deb@users.noreply.github.com>
2020-04-01 16:12:51 +0600

    added popup info

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ function updateMap(){
     .then(response => response.json())
     .then(rsp =>{
         console.log(rsp.data)
-        rsp.data.forEach(element => {
-            latitude = element.coordinates.latitude,
-            longitude = element.coordinates.longitude,
-            cases = element.latest_data.confirmed
+        rsp.data.forEach(array => {
+            latitude = array.coordinates.latitude,
+            longitude = array.coordinates.longitude,
+            cases = array.latest_data.confirmed,
+            countryName = array.name
             
             if (cases >= 255){
                 color="rgb(255,0,0)"
@@ -15,18 +16,37 @@ function updateMap(){
             else{
                 color = `rgb(${cases}, 0, 0)`
             }
+            
+            // let testingTemplate = "<style>h1{position:absolute; display: none; left:100px;}</style><h1>" + cases + "</h1>";
 
-            new mapboxgl.Marker({
+            var popup = new mapboxgl.Popup({
+                offset:[0, -70],
+                closeButton:false
+                }
+            ).setHTML(
+                "<h1>" + cases + countryName + "</h1>"
+            );
+
+            let marker = new mapboxgl.Marker({
                 draggable: false,
                 color: color
-                }).setLngLat([longitude, latitude])
-                .addTo(map);
+                })
+                .setLngLat([longitude, latitude])
+                .addTo(map)
+                marker.setPopup(popup);
+                
+            // get the marker element
+            const element = marker.getElement();
+            element.id = 'marker'
+            // hover event listener
+            element.addEventListener('mouseenter', () => popup.addTo(map));
+            element.addEventListener('mouseleave', () => popup.remove());
         });
+            
             // Add zoom and rotation controls to the map.
             map.addControl(new mapboxgl.NavigationControl());
             var geocoder = new MapboxGeocoder({
                 accessToken: mapboxgl.accessToken,
-                mapboxgl: mapboxgl
                 });
                  
             document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
